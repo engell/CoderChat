da07df88fbce9dd14ee58b9a22076616355685f3
Engell <angelpizano@xenodesystems.com>
2013-06-12 05:12:22 -0500

    Agregados efectos y funciones de lógica

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,11 +1,23 @@
 $(function() {
+   var message, nick;
+    nick = null;
+    if (nick === null) {
+      $("#alert").css({color: "red"});
+    } 
   return $("#button").on("click", (function() {
-    var message, user;
-    message = escape($("#message").val());
     nick = escape($("#nick").val());
-		if (message.indexOf("%3") !== -1) {
-			alert("No HTML");
-			return false; }
+		$("#nick").hide();
+		$("#yo").append(nick);
+		$("#yo").slideToggle();
+		if (nick != null) {
+			$("#alert").css({color: "white"});
+    }
+    message = escape($("#message").val());
+    if (message.indexOf("%3") !== -1) {
+      alert("No HTML");
+      return false;
+    }
     return $("#log").append("<li>" + nick + ": " + message + "</li>");
   }));
 });
+
